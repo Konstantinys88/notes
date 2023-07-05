683063e7fb8582cb64d04b90d2905de90dd7d45b
Maschkov Konstantin <maschkovc@yandex.ru>
2023-07-05 14:47:50 +0300

    Добавляем заметки

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -6,29 +6,56 @@ import AppFilter from '../app-filter/app-filter';
 import NotesList from '../notes-list/notes-list';
 import NotesAddForm from '../notes-add-form/notes-add-form';
 
+import { Component } from 'react';
 
 
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: [
+				{notes: "Проснутся" , priority: 70, increase: false, id: 1, like: false}, 
+				{notes: "Покормить кота" , priority: 70, increase: false, id: 2, like: false}, 
+				{notes: "Погладить кота" , priority: 90, increase: true, id: 3, like: false},
+				{notes: "Помыть посуду" , priority: 50, increase: false, id: 4, like: true},
+			],
+			// term: '',
+			// filter: 'all',
+		}
+		this.maxId = 5
+	}
 
-function App() {
+	addItem = (notes, priority) => {
+		const newItem = {
+			notes,
+			priority,
+			id: this.maxId++,
+			increase: false,
+			like: false,
+		}
+		this.setState(({ data }) => {
+			const newArr = [...data, newItem]; // в старый массив записываем новый обьект
+			return {
+				data: newArr
+			}
+		});
+	}
 
-	const data = [
-		{notes: "Проснутся" , priority: 70, increase: false, id: 1, like: false}, 
-		{notes: "Покормить кота" , priority: 70, increase: false, id: 2, like: false}, 
-		{notes: "Погладить кота" , priority: 90, increase: true, id: 3, like: false},
-		{notes: "Помыть посуду" , priority: 50, increase: false, id: 4, like: true},
-	];
-
-	return (
-		<div className="app">
-			<AppInfo />
-			<div className="app__search">
-				<SearchPanel />
-				<AppFilter />
+	render () {
+		const { data } = this.state;
+		return (
+			<div className="app">
+				<AppInfo />
+				<div className="app__search">
+					<SearchPanel />
+					<AppFilter />
+				</div>
+				<NotesList data={data}/>
+				<NotesAddForm onAddItem={this.addItem}/>
 			</div>
-			<NotesList data={data}/>
-			<NotesAddForm />
-		</div>
-	);
+		);
+
+	}
 }
 
 export default App;
